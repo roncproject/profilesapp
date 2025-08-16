2b26f9cccde925e0075436fc8a7b93ba03aad050
roncproject <ron.c.project@gmail.com>
2025-08-17 00:00:14 +0200

    vite svgr added

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+// https://stackoverflow.com/questions/70309561/unable-to-import-svg-with-vite-as-reactcomponent
+import svgr from 'vite-plugin-svgr' 
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+      svgr({ 
+        svgrOptions: {
+        // svgr options
+      },
+    }),
+  ],
 })
