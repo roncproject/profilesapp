8904fb998ceb37238ea6be54a493a0e8995075c6
roncproject <ron.c.project@gmail.com>
2025-08-25 23:56:45 +0200

    Cypress

---
+++ test/cypress/e2e/spec.cy.js
@@ -0,0 +1,11 @@
+describe('template spec', () => {
+  it('passes', () => {
+    // cy.visit('https://example.cypress.io')
+
+    cy.visit('https://main.d1esus7hyxwmru.amplifyapp.com/')
+
+
+
+
+  })
+})
